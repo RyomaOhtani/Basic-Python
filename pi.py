17c2a22dc9c1783e52ffa3c5dcf8f5060147b975
RyomaOhtani <jukenohtani@gmail.com>
2023-05-10 11:00:58 +0900

    add_assignments

--- pi.py
+++ pi.py
@@ -5,3 +5,13 @@ text = """
 
 # TODO
 
+# 句読点を除去し、単語リストを得る
+words = [word.strip(',.') for word in text.split()]
+
+# 各単語の文字数を得る
+char_counts = list(map(len, words))
+
+# くっつけて整数値に変換する
+num = int(''.join(map(str, char_counts)))
+
+print(num)
