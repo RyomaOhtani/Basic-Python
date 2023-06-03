3a6006f330c4a8a75b34798ba46cb1ba635477a1
RyomaOhtani <jukenohtani@gmail.com>
2023-06-03 16:51:49 +0900

    add_assignments

--- euclid.py
+++ euclid.py
@@ -9,9 +9,21 @@ def gcd(x, y):
         x, y = y, x % y
     return x
 
+# 値の維持
+aa = a
+bb = b
+
 # a > b になるように入れ替え
 if a < b:
     a, b = b, a
 
 GCD = gcd(a, b)
-print(GCD)
+
+# 元の値に戻す
+a=aa
+b=bb
+
+if GCD == 1:
+    print(a, "と", b, "は互いに素です")
+else:
+    print(a, "と", b, "の最大公約数は",GCD, "であるため,互いに素ではありません")
--- trapezoidal_integral.py
+++ trapezoidal_integral.py
@@ -1,18 +1,25 @@
-from math import sin, pi
+from sympy import symbols, sympify
 # --example--
 # print(sin(0))
 # >>> 0
 # -----------
 
-def f(x):
-    return sin(x)
+N = int(input("分割数を入力してください: ") or 100)
 
-N = 100
+# 実数以外（pi等）に対応するためsympifyを使いました
+a = sympify(input("積分区間の下限値を入力してください: ") or "0")
+b = sympify(input("積分区間の上限値を入力してください: ") or "1")
 
-a = 0
-b = 1 / 2 * pi
+# 変数xを定義
+x = symbols("x")
+
+expression = input("関数f(x)の式を入力してください: ")
+
+# 関数f(x)を定義
+f = sympify(expression)
+
+# 台形積分　sympyを用いたため、f.subsで変数置換を行った
 h = (b - a) / N
-
-S = (h / 2) * (f(a) + 2 * sum(f(a + h * i) for i in range(1, N)) + f(b))
-
-print(S)
+S = (h / 2) * (f.subs(x, a) + 2 * sum(f.subs(x, a + h * i) for i in range(1, N)) + f.subs(x, b)) 
+    
+print(float(S))
